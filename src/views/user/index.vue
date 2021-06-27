1bc3f21310615439b9711bfb5df85b8213b65043
五方 <imwyc@outlook.com>
2021-06-27 22:54:18 +0800

    add frist and second router components

---
+++ src/views/user/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="main">
+    <Header title="我的影院" />
+    <div id="content">
+      <login />
+    </div>
+    <TabBar />
+  </div>
+</template>
+<script>
+import Header from '@/components/header'
+import Login from '@/components/login'
+import TabBar from '@/components/tabBar'
+export default {
+  name: 'User',
+  components: {
+    Header,
+    Login,
+    TabBar
+  },
+  props: {},
+  data() {
+    return {}
+  },
+  activated() {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+<style scoped></style>
